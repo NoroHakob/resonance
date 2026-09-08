6a5a1cd42dd795030a3cf03f15cf84e1ba9773c0
NoroHakob <norohakob2004@gmail.com>
2026-09-09 00:58:52 +0400

    Backend Infrastructure

---
+++ src/trpc/routers/_app.ts
@@ -0,0 +1,15 @@
+import { 
+    baseProcedure, 
+    createTRPCRouter 
+} from '../init';
+export const appRouter = createTRPCRouter({
+  health: baseProcedure.query(async () => {
+    await new Promise((resolve) => setTimeout(resolve, 1000)); 
+
+    // throw new Error('Something went wrong!');
+
+    return { status: 'ok', code: 123 };
+  }),
+});
+// export type definition of API
+export type AppRouter = typeof appRouter;
